038af81db5341033f6e030b4f60635e0761e0f20
user1 <user1@IM01.phyuance.com>
2017-12-28 18:21:22 +0800

    censored-word test

--- cloud.js
+++ cloud.js
@@ -35,11 +35,11 @@ AV.Cloud.onIMMessageReceived((request) => {
     let processedContent = content;
    // console.log('content', processedContent);
     // 必须含有以下语句给服务端一个正确的返回，否则会引起异常
-  return {
+  return 
         _censored_words(url).then(res=>{
-             content: processedContent.replace('中介', '**')
+          content: res.data.map(item => processedContent.replace('中介', '**'))
        })
-  };
+  
 });
 
 
