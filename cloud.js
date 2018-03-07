16de5dd60c8168bd7f7278ea2a5a3a40a13593a1
user1 <user1@IM01.phyuance.com>
2018-03-07 17:29:06 +0800

    [phyuance-testing]update hook

--- cloud.js
+++ cloud.js
@@ -42,7 +42,6 @@ AV.Cloud.onIMMessageReceived((request) => {
     var check=0;
     console.log('***Start***');
     console.log('1.Content List:',tmp_content);
-    console.log('  lcattrs: ',tmp_content._lcattrs.conversationType);
     if(tmp_content._lctype<0){
         //black list
         
